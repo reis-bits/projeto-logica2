54dfc545f3667d26188a8bebb95cb9b037ea570e
Eduardo Reis <eduardorprincepe007@gmail.com>
2024-04-09 18:58:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-let titulo = document.querySelector('h1');
-let paragrafo = document.querySelector('p');
+let NumeroAleatorio = gerarNumeroAleatorio();
 const botaoVerificacao = document.querySelector('#verificar');
 
-titulo.innerHTML = 'Jogo do número secreto';
-paragrafo.innerHTML = 'Diga um número entre 1 e 10';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
+exibirTextoNaTela('h1', 'Jogo do número secreto')
+exibirTextoNaTela('p', 'Diga um número entre 1 e 10')
 
 botaoVerificacao.onclick = function verificarChute() {
-    console.log('O botão foi clicado');
+    let chute = document.querySelector('input').value;
+    console.log(chute == NumeroAleatorio);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1)
 }
